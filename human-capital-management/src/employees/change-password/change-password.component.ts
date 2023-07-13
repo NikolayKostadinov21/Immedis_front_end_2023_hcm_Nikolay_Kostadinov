9471ec9a64883a95468b14c36eebe84bcb1ddfdc
Nikolay Kostadinov <NikolayKostadinov21>
2023-07-13 13:50:11 +0300

    Implementing the password change verification and functionality check

--- human-capital-management/src/employees/change-password/change-password.component.ts
+++ human-capital-management/src/employees/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ChangePasswordService } from '../../shared/services/change-password.service';
 import { LocalStorageService } from '../../shared/services/local-storage.service';
 
@@ -10,28 +10,60 @@ import { LocalStorageService } from '../../shared/services/local-storage.service
 })
 export class ChangePasswordComponent {
 
+    newPassword = new FormControl(null, [
+        (c: AbstractControl) => Validators.required(c),
+        Validators.pattern(
+            /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+        ),
+    ]);
+    confirmPassword = new FormControl(null, [
+        (c: AbstractControl) => Validators.required(c),
+        Validators.pattern(
+            /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+        ),
+    ]);
+
     constructor(
         private changePasswordService: ChangePasswordService,
-        private localStorageService: LocalStorageService
-    ) {
+        private localStorageService: LocalStorageService,
+        private formBuilder: FormBuilder
+    ) { }
 
-    }
-
-    changePasswordForm = new FormGroup({
-        oldPassword: new FormControl('', {
-            validators: [Validators.required]
-        }),
-        newPassword: new FormControl('', {
-            validators: [Validators.required],
-        }),
-        confirmNewPassword: new FormControl('', {
-            validators: [Validators.required],
-        })
-    });
+    changePasswordForm = this.formBuilder.group(
+        {
+            newPassword: this.newPassword,
+            confirmPassword: this.confirmPassword,
+        },
+        {
+            validator: this.ConfirmedValidator('newPassword', 'confirmPassword'),
+        }
+    );
 
     onSubmit(): void {
+        if (!this.changePasswordForm?.valid) {
+            return;
+        }
+
         const currentUserId = this.localStorageService.getUser()?.id;
         const { newPassword } = this.changePasswordForm.value;
         this.changePasswordService.changeUserPassword(currentUserId!, newPassword!).subscribe();
+    }
+
+    ConfirmedValidator(controlName: string, matchingControlName: string) {
+        return (formGroup: FormGroup) => {
+            const control = formGroup.controls[controlName];
+            const matchingControl = formGroup.controls[matchingControlName];
+            if (
+                matchingControl.errors &&
+                !matchingControl.errors['confirmedValidator']
+            ) {
+                return;
+            }
+            if (control.value !== matchingControl.value) {
+                matchingControl.setErrors({ confirmedValidator: true });
+            } else {
+                matchingControl.setErrors(null);
+            }
+        };
     }
 }
